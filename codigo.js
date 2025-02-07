11e8fc352d54dbb289051ca954500e2b778b89ae
Franco Casafus <francocasafus55@gmail.com>
2025-02-07 01:57:40 -0300

    refactor: improved navbar design and made projects section responsive

--- codigo.js
+++ codigo.js
@@ -4,3 +4,14 @@ function bajar(){
         behavior: 'smooth'
     });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('[data-bs-toggle="popover"]').forEach(function (popoverEl) {
+        new bootstrap.Popover(popoverEl, {
+            placement: 'bottom', 
+            trigger: 'hover', 
+            customClass: 'custom-popover'
+        });
+    });
+});
+
